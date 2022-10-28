8221ab9eb1e1ba7cd07390dd3ce4cab8f6ab3b06
harshal-limaye <harshal.limaye.in@gmail.com>
2022-10-28 23:41:10 +0530

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,29 @@
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 import React from 'react';
+
 import 'prismjs/themes/prism-okaidia.css';
 import '../styles/index.css';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-xxxxxxxxxx"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.ANALYTICS_ID});
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
