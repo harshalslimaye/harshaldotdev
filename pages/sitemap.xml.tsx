bcec1bed385fea4038c0c8755043c04f6b575036
harshal-limaye <harshal.limaye.in@gmail.com>
2022-11-19 11:23:45 +0530

    correct sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -9,8 +9,8 @@ export const getServerSideProps = async ({ res }) => {
   let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   `;
-  const postEndpoint = `${process.env.IMAGE_DOMAIN}${process.env.POSTS_ENDPOINT}`;
-  const catEndpoint = `${process.env.IMAGE_DOMAIN}${process.env.CAT_ENDPOINT}`;
+  const postEndpoint = `http://${process.env.IMAGE_DOMAIN}/${process.env.POSTS_ENDPOINT}`;
+  const catEndpoint = `http://${process.env.IMAGE_DOMAIN}/${process.env.CAT_ENDPOINT}`;
   const posts: any = await axios.get(postEndpoint);
   const categories: any = await axios.get(catEndpoint);
 
